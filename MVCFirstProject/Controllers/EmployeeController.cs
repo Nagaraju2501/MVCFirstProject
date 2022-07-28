68bd717620ceac6ef961f14c831075b8751d3be0
Nagaraju2501 <109521333+Nagaraju2501@users.noreply.github.com>
2022-07-28 12:40:05 +0530

    Initial commit

---
+++ MVCFirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCFirstProject.Models;
+
+
+namespace MVCFirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 213495,
+                EmployeeName = "Nagaraju",
+                EmployeeLocation = "Bangalore"
+            };
+
+
+
+
+            return View(employee);
+        }
+    }
+}
+
